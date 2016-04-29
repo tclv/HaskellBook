621d48ed3bf4d213c998fef7e13d550d035f1aee
Dorus Leliveld <dorusleliveld@gmail.com>
2016-04-29 11:01:42 +0200

    Progress ch11 exc

--- ch11/exercises.hs
+++ ch11/exercises.hs
@@ -11,4 +11,36 @@ data Garden =
      deriving Show
 -- Cant distribute over tabes as this is simply (a * b) 
 
-     
+-- Generate all programmer possibilities 
+
+data OperatingSystem =
+       GnuPlusLinux
+     | OpenBSDPlusNevermindJustBSDStill
+     | Mac
+     | Windows
+     deriving (Eq, Show)
+
+data ProgrammingLanguage =
+       Haskell
+     | Agda
+     | Idris
+     | PureScript
+     deriving (Eq, Show)
+
+data Programmer =
+  Programmer { os :: OperatingSystem
+             , lang :: ProgrammingLanguage }
+           deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem] 
+allOperatingSystems = [ GnuPlusLinux
+                      , OpenBSDPlusNevermindJustBSDStill 
+                      , Mac
+                      , Windows 
+                      ]
+allLanguages :: [ProgrammingLanguage] 
+allLanguages = [Haskell, Agda, Idris, PureScript]
+
+
+allProgrammers :: [Programmer]
+allProgrammers = [Programmer x y | x <- allOperatingSystems, y <- allLanguages]
